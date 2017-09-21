eada4ad4d2aa311f0bf4d34779509b3932e425bc
Gurinder Singh <gsingh622@yahoo.com>
2017-09-21 01:06:36 -0400

    Add files via upload

---
+++ www/CordovaMapSwift.js
@@ -0,0 +1,28 @@
+var exec = require('cordova/exec');
+
+exports.coolMethod = function(arg0, success, error) {
+    exec(success, error, "CordovaMapSwift", "coolMethod", [arg0]);
+};
+
+exports.echo = function(arg0, success, error) {
+  exec(success, error, 'CordovaMapSwift', 'echo', [arg0]);
+};
+
+exports.echojs = function(arg0, success, error) {
+  if (arg0 && typeof(arg0) === 'string' && arg0.length > 0) {
+    success(arg0);
+  } else {
+    error('Empty message!');
+  }
+};
+
+exports.showMap = function(arg0, success, error) {
+    exec(success, error,  "CordovaMapSwift", "show", [arg0]);
+};
+
+exports.showMap = function(arg0, success, error) {
+    exec(success, error,  "CordovaMapSwift", "hide", [arg0]);
+};  
+
+
+
